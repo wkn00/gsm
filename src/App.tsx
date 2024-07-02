2b224cf0fce314970e181d24687a18a905f5414b
Wael Kattan <waelk@uia.no>
2024-07-02 04:58:54 +0200

    removed basedomain

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Game from './components/Game';
 
 const App: React.FC = () => {
     return (
-        <Router basename='/gsm'>
+        <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/create" element={<CreateGame />} />
